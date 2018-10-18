c89cec03486dc37b300a9e46f5052ac1593be207
HaydenCarroll <44235446+HaydenCarroll@users.noreply.github.com>
2018-10-18 11:31:14 -0700

    Add files via upload

---
+++ FiveStar/src/Manager/Material.java
@@ -0,0 +1,10 @@
+package Manager;
+
+//(Andres R) This class should contain a name, price per foot(what the customer is charged), cost per unit,
+//total amount spent on the material, total footage of material sold, total amount sold of material(money amnt),
+//make a method to update total spent, total footage,
+//total amount of material(ex. updateSold(number) so that sold=sold+number)
+//getters and setters
+public class Material {
+
+}
