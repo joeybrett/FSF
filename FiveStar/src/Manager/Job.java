4702a82ad1e3e03422ad73f70680b00f5f12f552
HaydenCarroll <44235446+HaydenCarroll@users.noreply.github.com>
2018-10-18 11:32:51 -0700

    Update Job.java

--- FiveStar/src/Manager/Job.java
+++ FiveStar/src/Manager/Job.java
@@ -1,7 +1,7 @@
 package Manager;
 
 //(Andres G) this class is equivalent to an order and should include the customer id, job id, address, 
-//quote, date(i will set this part up when i finish the ui so dont worry about it), supplies used, supplies cost,
+//quote, date(i will set this part up when i finish the ui so dont worry about it), Material,
 //labor cost, the crew, and a boolean of if it has been completed.
 //there could be a method used to assign the job id that uses the Data class to find the value (ex. Data.getNextJobId() )
 //getters and setters
