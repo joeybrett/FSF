90b51cc8079598ab2bd6f3c2919704cd34222801
joeybrett <43344245+joeybrett@users.noreply.github.com>
2018-10-18 14:34:02 -0400

    Update Customer.java

--- FiveStar/src/Manager/Customer.java
+++ FiveStar/src/Manager/Customer.java
@@ -4,5 +4,55 @@ package Manager;
 //use the data class to find the customer id (Data.getNextCustomerId()) 
 //getters and setters
 public class Customer {
-
+	int adID;
+	String name;
+	Address address;
+	int numOfJobs;
+	String billingAddress;
+	String email;
+	public Customer(int id,int adId,String name,Address address,int numOfJobs,String billingAddress,String email) {
+		id=Data.getNextCustomerId();
+		this.adID=adId;
+		this.name=name;
+		this.address=address;
+		this.numOfJobs=numOfJobs;
+		this.billingAddress=billingAddress;
+		this.email=email;
+	}
+	public int getAdID() {
+		return adID;
+	}
+	public void setAdID(int adID) {
+		this.adID = adID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public int getNumOfJobs() {
+		return numOfJobs;
+	}
+	public void setNumOfJobs(int numOfJobs) {
+		this.numOfJobs = numOfJobs;
+	}
+	public String getBillingAddress() {
+		return billingAddress;
+	}
+	public void setBillingAddress(String billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
